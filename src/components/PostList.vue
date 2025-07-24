4340d867c8cc51234ae640de6e5e49833e7cc050
Thefang <178805417@qq.com>
2025-07-24 20:35:53 +0800

    feat(frontend): 云搜项目初始化
    1. 初始页面布局
    2. 自定义全局请求响应拦截器

---
+++ src/components/PostList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="post-list">
+    <a-list item-layout="horizontal" :data-source="props.postList">
+      <template #renderItem="{ item }">
+        <a-list-item>
+          <a-list-item-meta :description="item.content">
+            <template #title>
+              <a href="">{{ item.title }}</a>
+            </template>
+            <template #avatar>
+              <a-avatar :src="ding" />
+            </template>
+          </a-list-item-meta>
+        </a-list-item>
+      </template>
+    </a-list>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ding from "../assets/dingding.png";
+import { defineProps, withDefaults } from "vue";
+
+interface Props {
+  postList: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  postList: () => [],
+});
+</script>
+
+<style scoped>
+.ding {
+  width: 200px;
+}
+</style>
